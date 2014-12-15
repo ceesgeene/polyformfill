261172f6327a84c053c079c174297b95b5299211
ceesgeene <ceesgeene@gmail.com>
2014-12-15 02:20:48 +0100

    build polyformfill.js and polyformfill.js using a prefix and a suffix file

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,36 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
+'use strict';
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  replace = require('gulp-replace'),
+  sourcemaps = require('gulp-sourcemaps'),
+  uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
   // place code for your default task here
 });
 
 gulp.task('compress', function () {
-  gulp.src('polyfill-input-datetime.js')
+  gulp.src(['src/polyformfill.js.prefix', 'src/**/*.js', 'src/polyformfill.js.suffix'])
+    .pipe(concat('polyformfill.js'))
+    .pipe(replace("'use strict';", ''))
+    .pipe(uglify({
+      mangle: false,
+      output: {
+        indent_level: 2,
+        beautify: true,
+        bracketize: true,
+        semicolons: false,
+        width: 120
+      },
+      compress: false,
+      preserveComments: 'some'
+    }))
+    .pipe(gulp.dest('./'))
     .pipe(sourcemaps.init())
     .pipe(uglify())
-    .pipe(rename('polyfill-input-datetime.min.js'))
+    .pipe(rename('polyformfill.min.js'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
 });
