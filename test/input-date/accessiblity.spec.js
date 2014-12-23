17fa16126d25164e59915a52712e3a42f9cc8c0d
ceesgeene <ceesgeene@gmail.com>
2014-12-23 23:20:23 +0100

    Tests for Tab and Backspace keydown

--- test/input-date/accessiblity.spec.js
+++ test/input-date/accessiblity.spec.js
@@ -1,36 +1,8 @@
 'use strict';
 
+/* global describe, expect, it */
+
 describe('An accessible input[type=date] element', function () {
-
-  /*it('should have its value cleared on BACKSPACE keydown event', function () {
-    var input = document.createElement('input');
-    input.setAttribute('type', 'date');
-    input.setAttribute('value', '1970-01-01');
-
-    document.body.appendChild(input);
-
-    expect(input.value).toBe('1970-01-01');
-
-    input.dispatchEvent(window.crossBrowser_initKeyboardEvent("keydown", {"key": 'Backspace', 'bubbles': true}));
-
-    expect(input.value).toBe('');
-  });*/
-
-  /*it('can be navigated using the arrow LEFT and arrow RIGHT keys', function () {
-      var input = document.createElement('input');
-      input.setAttribute('type', 'date');
-      input.setAttribute('value', '1970-01-01');
-
-      document.body.appendChild(input);
-
-      expect(input.value).toBe('1970-01-01');
-
-      input.focus();
-      input.dispatchEvent(window.crossBrowser_initKeyboardEvent("keydown", { "key": 'Right', 'bubbles': true }));
-      input.dispatchEvent(window.crossBrowser_initKeyboardEvent("keydown", { "key": 'Up', 'bubbles': true }));
-
-      expect(input.value).toBe('1970-02-01');
-  });*/
 
   describe('can be navigated using the arrow keys', function () {
     var w;
@@ -100,5 +72,80 @@ describe('An accessible input[type=date] element', function () {
   });
 
 
-});
+  describe('can be navigated using Tab and Shift+Tab keydown', function () {
+    var w;
 
+    it('test 1', function (done) {
+
+      window.hello = function () {
+
+        var inputBefore = w.document.createElement('input');
+        inputBefore.setAttribute('type', 'text');
+
+        var input = w.document.createElement('input');
+        input.setAttribute('type', 'date');
+        input.setAttribute('value', '1007-06-15');
+
+        w.document.body.appendChild(inputBefore);
+        w.document.body.appendChild(input);
+
+        inputBefore.focus();
+        inputBefore.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'U+0009', 'bubbles': true}));
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'U+0009', 'bubbles': true}));
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'Up', 'bubbles': true}));
+
+        expect(input.value).toBe('1007-07-15');
+
+        setTimeout(function () {
+          w.close();
+          done();
+        }, 100);
+      };
+
+      w = window.open('base/test/popup.html', 'test');
+    });
+  });
+
+  describe('allows its date components to be cleared on Backspace or Del keydown', function () {
+    var w;
+
+    it('test 1', function (done) {
+
+      window.hello = function () {
+
+        var input = w.document.createElement('input');
+        input.setAttribute('type', 'date');
+        input.setAttribute('value', '1007-06-15');
+
+        w.document.body.appendChild(input);
+
+        input.focus();
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'Right', 'bubbles': true}));
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'U+0008', 'bubbles': true}));
+
+        expect(input.value).toBe('');
+
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'Up', 'bubbles': true}));
+
+        expect(input.value).toBe('1007-01-15');
+
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'Right', 'bubbles': true}));
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'U+0008', 'bubbles': true}));
+
+        expect(input.value).toBe('');
+
+        input.dispatchEvent(w.crossBrowser_initKeyboardEvent("keydown", {"key": 'Up', 'bubbles': true}));
+
+        expect(input.value).toBe('1007-01-01');
+
+        setTimeout(function () {
+          w.close();
+          done();
+        }, 100);
+      };
+
+      w = window.open('base/test/popup.html', 'test');
+    });
+  });
+
+});
